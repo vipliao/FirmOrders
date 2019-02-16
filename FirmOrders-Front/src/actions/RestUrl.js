9a3146e42f69b3aaa8a4bc1405f5ff9bf84bc18e
lsx9985 <lsx9985@163.com>
2019-02-16 20:58:00 +0800

    提交代码

--- FirmOrders-Front/src/actions/RestUrl.js
+++ FirmOrders-Front/src/actions/RestUrl.js
@@ -1,6 +1,4 @@
-// const ADDR = 'http://118.31.44.54:8899';
-const ADDR = 'http://47.92.82.33:8080';
-//const ADDR = 'http://127.0.0.1:8080';
+const ADDR = 'http://127.0.0.1:8080';
 
 module.exports = {
     ADDR,
