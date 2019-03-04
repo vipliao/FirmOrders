176b933426bd57b188b7b42dae73097dca64be3f
vipwords <vipwords@163.com>
2019-03-04 13:18:04 +0800

    提交代码:修改cors

--- FirmOrders-Boot/src/main/java/com/firm/order/config/cors/CorsConfig.java
+++ FirmOrders-Boot/src/main/java/com/firm/order/config/cors/CorsConfig.java
@@ -1,22 +1,48 @@
 package com.firm.order.config.cors;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer  {
+public class CorsConfig implements Filter {
+
 	@Value("${httpheader.authorization}")
 	private String authorization;
 	
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowCredentials(true)
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("x-requested-with,Access-Control,Content-Type,"+authorization)
-            .maxAge(3600);
-    }
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+		httpResponse.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE");
+		httpResponse.setHeader("Access-Control-Max-Age", "3600");
+		httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+		httpResponse.setHeader("Access-Control-Allow-Headers",
+				"x-requested-with,Access-Control,Content-Type,x-auth-token");
+		//httpResponse.setHeader("Access-Control-Expose-Headers","Content-Disposition");
+
+		chain.doFilter(request, response);
+
+	}
+
+	@Override
+	public void destroy() {
+	}
 }
