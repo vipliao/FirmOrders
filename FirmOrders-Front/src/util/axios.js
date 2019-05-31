048eb617f74d8a07dfe734f60cb30d7ddf5837cd
vipwords <vipwords@163.com>
2019-05-31 17:22:28 +0800

    提交代码: 修改axios.js

--- FirmOrders-Front/src/util/axios.js
+++ FirmOrders-Front/src/util/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import restUrl from 'RestUrl';
+import {Encrypt,Sign} from "Utils/encrypt";
+
+
 
 axios.defaults.baseURL = restUrl.BASE_HOST;
 axios.defaults.headers['Content-Type'] = 'application/json;charset=UTF-8';
@@ -7,10 +10,33 @@ axios.defaults.headers['Content-Type'] = 'application/json;charset=UTF-8';
 // 添加请求拦截器
 axios.interceptors.request.use(config => {
     // 在发送请求之前做些什么
+
+    // 数据加密
+    /*if (config.data) {
+        try {
+            config.data =Encrypt(JSON.stringify(config.data)).replace(/\+/g, '%')
+        } catch (e) {
+            console.error('Encryption failed')
+        }
+    }*/
+    // 签名串
+    let timestamp = new Date().getTime();
+    //设置token
+
     const token = sessionStorage.getItem('token');
     if(token){
         config.headers['X-Auth-Token'] = `${token}`;
     }
+  /*  if(token){
+        let signstr = Sign(token, timestamp, config.data);
+        console.log('token', token);
+        console.log('signstr', signstr);
+        config.headers = {
+            timestamp,
+            signstr
+        };
+    }*/
+
     return config;
 }, error => {
     // 对请求错误做些什么
