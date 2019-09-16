06662454e72d8819c0a5c413a7840a38e1d0e0e2
lsx9985 <lsx9985@163.com>
2019-09-16 22:40:57 +0800

    提交代码

--- FirmOrders-Boot/src/main/java/com/firm/order/utils/SymmetricEncoder.java
+++ FirmOrders-Boot/src/main/java/com/firm/order/utils/SymmetricEncoder.java
@@ -37,7 +37,9 @@ public class SymmetricEncoder {
 			KeyGenerator keygen = KeyGenerator.getInstance("AES");
 			// 2.根据ecnodeRules规则初始化密钥生成器
 			// 生成一个128位的随机源,根据传入的字节数组
-			keygen.init(128, new SecureRandom(encodeRules.getBytes()));
+			SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+			secureRandom.setSeed(encodeRules.getBytes());
+			keygen.init(128, secureRandom);
 			// 3.产生原始对称密钥
 			SecretKey original_key = keygen.generateKey();
 			// 4.获得原始对称密钥的字节数组
@@ -91,7 +93,9 @@ public class SymmetricEncoder {
 			KeyGenerator keygen = KeyGenerator.getInstance("AES");
 			// 2.根据ecnodeRules规则初始化密钥生成器
 			// 生成一个128位的随机源,根据传入的字节数组
-			keygen.init(128, new SecureRandom(encodeRules.getBytes()));
+			SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+			secureRandom.setSeed(encodeRules.getBytes());
+			keygen.init(128, secureRandom);
 			// 3.产生原始对称密钥
 			SecretKey original_key = keygen.generateKey();
 			// 4.获得原始对称密钥的字节数组
