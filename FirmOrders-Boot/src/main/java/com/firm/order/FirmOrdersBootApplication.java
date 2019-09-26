29bc330280eb4c3a089f0f5f0ba4ffc54785e039
lsx9985 <lsx9985@163.com>
2019-09-26 14:26:09 +0800

    提交代码

--- FirmOrders-Boot/src/main/java/com/firm/order/FirmOrdersBootApplication.java
+++ FirmOrders-Boot/src/main/java/com/firm/order/FirmOrdersBootApplication.java
@@ -1,30 +1,108 @@
 package com.firm.order;
 
+import com.firm.order.config.context.DefaultProfileUtil;
+import com.firm.order.config.propertyresolver.FirmEncryptablePropertyResolver;
+import com.firm.order.config.propertyresolver.FrimEncryptablePropertyDetector;
+import com.firm.order.utils.SpringProfileConstants;
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyDetector;
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyResolver;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
-import com.firm.order.config.propertyresolver.FirmEncryptablePropertyResolver;
-import com.firm.order.config.propertyresolver.FrimEncryptablePropertyDetector;
-import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyDetector;
-import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyResolver;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
 
 @SpringBootApplication
 @ServletComponentScan
+@Slf4j
 //@ImportResource(locations={"classpath:application-quartz.xml"})
-public class FirmOrdersBootApplication {
-	@Bean
+public class FirmOrdersBootApplication implements InitializingBean {
+
+    private final Environment env;
+
+    @Bean
     public EncryptablePropertyDetector encryptablePropertyDetector() {
         return new FrimEncryptablePropertyDetector();
     }
-	@Bean
-	public EncryptablePropertyResolver encryptablePropertyResolver() {
-		return new FirmEncryptablePropertyResolver();
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(FirmOrdersBootApplication.class, args);
-	}
 
+    @Bean
+    public EncryptablePropertyResolver encryptablePropertyResolver() {
+        return new FirmEncryptablePropertyResolver();
+    }
+
+    public FirmOrdersBootApplication(Environment env) {
+        this.env = env;
+    }
+
+    /**
+     * Initializes nfccanteen.
+     * <p>
+     * Spring profiles can be configured with a program argument --spring.profiles.active=your-active-profile
+     * <p>
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+        if (activeProfiles.contains(SpringProfileConstants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(SpringProfileConstants.SPRING_PROFILE_PRODUCTION)) {
+            log.error("You have misconfigured your application! It should not run " +
+                    "with both the 'dev' and 'prod' profiles at the same time.");
+        }
+        if (activeProfiles.contains(SpringProfileConstants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(SpringProfileConstants.SPRING_PROFILE_CLOUD)) {
+            log.error("You have misconfigured your application! It should not " +
+                    "run with both the 'dev' and 'cloud' profiles at the same time.");
+        }
+    }
+
+    /**
+     * Main method, used to run the application.
+     *
+     * @param args the command line arguments.
+     */
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(FirmOrdersBootApplication.class);
+        DefaultProfileUtil.addDefaultProfile(app);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles());
+    }
 }
 
